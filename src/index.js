e163208b5d7b358f0497e7e0247c3e7c1d8a1614
Ungweru Phiri <phiriungweru@gmail.com>
2025-12-13 11:31:58 +0200

    Successful test for the local storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { todoListItem } from "./todoListItem";
 import { project } from "./projects";
 import { projectList } from "./projectList";
+import { storeData, readData, removeData } from "./storage";
 
 const homework = todoListItem("Homework", "This is my science homework", "15th march 2026", "very important");
 const poetry = todoListItem("Poetry", "This is a famous piece written by shakespare", "16th march 2026", "medium");
@@ -10,6 +11,14 @@ const dished = todoListItem("Plates", "This is my science homework", "15th march
 const washing = todoListItem("clothes", "This is a famous piece written by shakespare", "16th march 2026", "medium");
 const cleaning = project("Home Work", dished, washing);
 
-console.log(projectList(chores, cleaning));
+const data = projectList(chores, cleaning);
+
+storeData('demoData', data);
+
+const storedData = readData('demoData');
+
+console.log("This is the stored data: ", storedData);
+// console.log("Data:", data);
+// console.log(projectList(chores, cleaning));
 // console.log(chores);
 // console.log(newNote);
